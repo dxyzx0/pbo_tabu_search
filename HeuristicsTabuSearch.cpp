296cadd040f50ef436a32ce77b077465fc9f8cb2
psw <2019212802@live.sufe.edu.cn>
2024-05-24 17:04:35 +0200

    tmp commit

--- HeuristicsTabuSearch.cpp
+++ HeuristicsTabuSearch.cpp
@@ -3,7 +3,54 @@
 //
 
 #include "HeuristicsTabuSearch.h"
+#include <ranges>
+
 HeurResult HeuristicsTabuSearch::heuristic()
 {
-	return HeuristicsRandom::heuristic();
+	HeurResult res = HeurResult::HEUR_NOTFIND;
+	for (long i = 0; i < nTryPerRound; i++)
+	{
+//		shared_ptr< IntVec > move = gen_rnd_vec(prob->nVar, 2);
+//		long hash = move->at(0) * prob->nVar + move->at(1);
+//		if (tabuList.find(hash) == tabuList.end() || tabuList[hash] < i)
+//		{
+//			HeurResult res = prob->evaluate_move(move);
+//			if (res.obj < prob->objVal)
+//			{
+//				prob->update_move(move);
+//				prob->objVal = res.obj;
+//				tabuList[hash] = i + nTabuTenure;
+//			}
+//		}
+	}
+
+	return res;
+}
+
+tuple< shared_ptr< IntVec >, shared_ptr< IntVec > > HeuristicsTabuSearch::calAxb(IntVec& x)
+{
+	auto A_ineq = prob->getA_ineq();
+	auto A_eq = prob->getA_eq();
+
+	auto b_ineq = prob->getB_ineq();
+	auto b_eq = prob->getB_eq();
+
+	auto Ax_b_ineq = make_shared< IntVec >((*A_ineq) * x - (*b_ineq));
+	auto Ax_b_eq = make_shared< IntVec >((*A_eq) * x - (*b_eq));
+
+	return make_tuple(Ax_b_ineq, Ax_b_eq);
+}
+
+bool HeuristicsTabuSearch::isFeasible(IntVec& Ax_b_ineq, IntVec& Ax_b_eq)
+{
+	// test if Ax_b_ineq >= 0 and Ax_b_eq == 0
+	if (!ranges::all_of(Ax_b_eq, [](auto i) { return i == 0; }))
+	{
+		return false;
+	}
+	if (!ranges::all_of(Ax_b_ineq, [](auto i) { return i >= 0; }))
+	{
+		return false;
+	}
+	return true;
 }
--- HeuristicsTabuSearch.h
+++ HeuristicsTabuSearch.h
@@ -7,11 +7,23 @@
 
 #include "HeuristicsRandom.h"
 
-class HeuristicsTabuSearch: public HeuristicsRandom
-{
- public:
-  HeurResult heuristic() override;
+#include <algorithm>
 
+using namespace std;
+
+class HeuristicsTabuSearch : public HeuristicsRandom
+{
+	 protected:
+	 long nTabuListMax = 10;
+	 long nTabuTenure = 10;
+	 unordered_map< long, size_t > tabuList;  // key: hash of move, value: the iteration when the move is allowed
+	 IntegerType w_den = 0;
+	 IntegerType w_num = 1;
+ public:
+	HeurResult heuristic() override;
+	tuple< shared_ptr< IntVec >, shared_ptr< IntVec > > calAxb(IntVec& x);
+	bool isFeasible(IntVec& Ax_b_ineq, IntVec& Ax_b_eq);
+	shared_ptr< IntVec > score(IntVec& x, IntVec& Ax);
 };
 
 #endif //PBO_HEURISTICS__HEURISTICSTABUSEARCH_H_
